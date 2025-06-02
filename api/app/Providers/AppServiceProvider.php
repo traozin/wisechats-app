7995dc93e9d568589e9b9fc9ec1062d1b0155dcb
Antônio Neto <acm.neto1999@gmail.com>
2025-06-02 20:30:05 -0300

    :sparkles: feat: adiciona configuração de rotas da API e implementa endpoint /ping

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -3,22 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
+    public function register(): void {
         //
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot(): void {
+        Route::prefix('api/v1')
+            ->middleware([
+                'api'
+            ])
+            ->group(base_path('routes/api.php'));
     }
 }
