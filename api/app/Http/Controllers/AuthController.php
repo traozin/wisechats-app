d7e5a1e72b88d830e77c13908f27b29d26435229
Antônio Neto <acm.neto1999@gmail.com>
2025-06-03 17:51:32 -0300

    :bug: fix: melhora a validação de dados no login e ajusta o tratamento de erros para credenciais inválidas

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -5,27 +5,34 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Routing\Controller;
 
 class AuthController extends Controller {
 
     public function login(Request $request) {
-        if (!$request->all()) {
-            return response()->json(['error' => 'Dados não enviados.'], 400);
-        }
 
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Dados inválidos',
+                'errors' => $validator->errors()
+                ], 422
+            );
+        }
+
         $user = User::where('email', $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['As credenciais estão incorretas.'],
-            ]);
+            return response()->json([
+                'message' => 'Credenciais inválidas',
+                'errors' => ['email' => ['As credenciais informadas estão incorretas.']]
+            ], 422);
         }
 
         return response()->json([
