b34e96507212ed5a45955f1681d70a179e7f3cf5
Antônio Neto <acm.neto1999@gmail.com>
2025-06-02 21:54:12 -0300

    :sparkles: feat: adiciona validações de Uuid e regras de existência para user_id e product_id nas classes OrderData e OrderItemData

--- api/app/Data/OrderData.php
+++ api/app/Data/OrderData.php
@@ -2,13 +2,24 @@
 
 namespace App\Data;
 
+use Spatie\LaravelData\Attributes\DataCollectionOf;
+use Spatie\LaravelData\Attributes\Validation\Rule;
+use Spatie\LaravelData\Attributes\Validation\Uuid;
+use Spatie\LaravelData\Attributes\Validation\Required;
+use Spatie\LaravelData\Attributes\Validation\Min;
 use Spatie\LaravelData\Data;
+
 use App\Data\OrderItemData;
 
 class OrderData extends Data {
     public function __construct(
+        #[Uuid]
+        #[Rule('exists:users,id')]
         public string $user_id,
-        /** @var OrderItemData[] */
+
+        #[Required]
+        #[Min(1)]
+        #[DataCollectionOf(OrderItemData::class)]
         public array $items
     ) {
     }
--- api/app/Data/OrderItemData.php
+++ api/app/Data/OrderItemData.php
@@ -3,10 +3,17 @@
 namespace App\Data;
 
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Attributes\Validation\Uuid;
+use Spatie\LaravelData\Attributes\Validation\Min;
+use Spatie\LaravelData\Attributes\Validation\Rule;
 
 class OrderItemData extends Data {
     public function __construct(
+        #[Uuid]
+        #[Rule('exists:products,id')]
         public string $product_id,
+
+        #[Min(1)]
         public int $quantity
     ) {
     }
