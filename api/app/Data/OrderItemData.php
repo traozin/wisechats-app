d03552367d013a7bfe62df12575cf5c2929744ea
Antônio Neto <acm.neto1999@gmail.com>
2025-06-03 00:07:31 -0300

    :bug: fix: corrige tipo de dado de product_id para int e remove validação de UUID

--- api/app/Data/OrderItemData.php
+++ api/app/Data/OrderItemData.php
@@ -9,9 +9,8 @@ use Spatie\LaravelData\Attributes\Validation\Rule;
 
 class OrderItemData extends Data {
     public function __construct(
-        #[Uuid]
         #[Rule('exists:products,id')]
-        public string $product_id,
+        public int $product_id,
 
         #[Min(1)]
         public int $quantity
