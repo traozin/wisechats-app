b7d0c71123c35b58a0c7376d412209c2558e0f09
Antônio Neto <acm.neto1999@gmail.com>
2025-06-03 23:55:59 -0300

    :sparkles: feat: adiciona página de registro com formulário de cadastro com integração com o backend

---
+++ web/src/app/(pages)/register/page.tsx
@@ -0,0 +1,22 @@
+import { RegisterForm } from "@/components/register-form"
+
+export default function RegisterPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <RegisterForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/placeholder.svg?height=800&width=600"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
+}
