df290482c53ef7c76fb0c3c90fb4c9f00e169feb
Antônio Neto <acm.neto1999@gmail.com>
2025-06-03 22:06:09 -0300

    :sparkles: feat: implementação da tela de login

--- web/src/components/login-form.tsx
+++ web/src/components/login-form.tsx
@@ -10,36 +10,36 @@ export function LoginForm({
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <h1 className="text-2xl font-bold">Entrar na sua conta</h1>
         <p className="text-muted-foreground text-sm text-balance">
-          Enter your email below to login to your account
+          Digite seu e-mail abaixo para acessar sua conta
         </p>
       </div>
       <div className="grid gap-6">
         <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Label htmlFor="email">E-mail</Label>
+          <Input id="email" type="email" placeholder="m@exemplo.com" required />
         </div>
         <div className="grid gap-3">
           <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
+            <Label htmlFor="password">Senha</Label>
             <a
               href="#"
               className="ml-auto text-sm underline-offset-4 hover:underline"
             >
-              Forgot your password?
+              Esqueceu sua senha?
             </a>
           </div>
           <Input id="password" type="password" required />
         </div>
         <Button type="submit" className="w-full">
-          Login
+          Entrar
         </Button>
       </div>
       <div className="text-center text-sm">
-        Don&apos;t have an account?{" "}
-        <a href="/register" className="underline underline-offset-4">
-          Sign up
+        Não tem uma conta?{" "}
+        <a href="#" className="underline underline-offset-4">
+          Cadastre-se
         </a>
       </div>
     </form>
