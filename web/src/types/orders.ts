2c55132a49266ffed49bd59aab396051c6580f98
Antônio Neto <acm.neto1999@gmail.com>
2025-06-04 21:43:08 -0300

    :sparkles: feat: implementando integração com API da rota da grid de orders

---
+++ web/src/types/orders.ts
@@ -0,0 +1,30 @@
+export interface Orders {
+  id: string;
+  user_id: string;
+  total: number;
+  items: Array<Items>;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface Items {
+  id: string;
+  order_id: string;
+  product_id: string;
+  quantity: number;
+  unit_price: number;
+  subtotal: number;
+  created_at: Date;
+  updated_at: Date;
+  product: Product;
+}
+
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  created_at: Date;
+  updated_at: Date;
+}
