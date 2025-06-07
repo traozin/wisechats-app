26de5e99226cf72e203ceb4c0fa99f374f917505
Antônio Neto <acm.neto1999@gmail.com>
2025-06-06 22:20:20 -0300

    :sparkles: feat: adicionando informações do usuário logado

--- web/src/components/site/app-sidebar.tsx
+++ web/src/components/site/app-sidebar.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 import {
   ArrowUpCircleIcon,
   Container,
@@ -8,11 +8,11 @@ import {
   SearchIcon,
   SettingsIcon,
   UserIcon,
-} from "lucide-react"
+} from "lucide-react";
 
-import { NavMain } from '@/components/site/nav-main'
-import { NavSecondary } from '@/components/site/nav-secondary'
-import { NavUser } from '@/components/site/nav-user'
+import { NavMain } from "@/components/site/nav-main";
+import { NavSecondary } from "@/components/site/nav-secondary";
+import { NavUser } from "@/components/site/nav-user";
 import {
   Sidebar,
   SidebarContent,
@@ -21,46 +21,65 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from '@/components/ui/sidebar'
-
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  navMain: [
-    {
-      title: "Pedidos",
-      url: "/home",
-      icon: Container,
-    },
-    {
-      title: "Usuários",
-      url: "/users",
-      icon: UserIcon,
-    },
-    {
-      title: "Produtos",
-      url: "/products",
-      icon: ListIcon,
-    }
-  ],
-  navSecondary: [
-    {
-      title: "Settings",
-      url: "/home",
-      icon: SettingsIcon,
-    },
-    {
-      title: "Search",
-      url: "#",
-      icon: SearchIcon,
-    },
-  ],
-}
+} from "@/components/ui/sidebar";
+import { UserService } from "@/hooks/user-hook";
+import { useEffect, useState } from "react";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const [userName, setUserName] = useState<string>("");
+  const [userEmail, setUserEmail] = useState<string>("");
+
+  const data = {
+    user: {
+      name: userName,
+      email: userEmail,
+      avatar: "/avatars/shadcn.jpg",
+    },
+    navMain: [
+      {
+        title: "Pedidos",
+        url: "/home",
+        icon: Container,
+      },
+      {
+        title: "Usuários",
+        url: "/users",
+        icon: UserIcon,
+      },
+      {
+        title: "Produtos",
+        url: "/products",
+        icon: ListIcon,
+      },
+    ],
+    navSecondary: [
+      {
+        title: "Settings",
+        url: "/home",
+        icon: SettingsIcon,
+      },
+      {
+        title: "Search",
+        url: "#",
+        icon: SearchIcon,
+      },
+    ],
+  };
+
+  async function fetchUser() {
+    try {
+      const user = (await UserService.getMe()).user;
+      setUserName(user.name ?? "");
+      setUserEmail(user.email ?? "");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <Sidebar collapsible="offcanvas" {...props}>
       <SidebarHeader>
@@ -68,8 +87,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenuItem>
             <SidebarMenuButton
               asChild
-              className="data-[slot=sidebar-menu-button]:!p-1.5"
-            >
+              className="data-[slot=sidebar-menu-button]:!p-1.5">
               <a href="#">
                 <ArrowUpCircleIcon className="h-5 w-5" />
                 <span className="text-base font-semibold">WiseChats.ai.</span>
@@ -86,5 +104,5 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavUser user={data.user} />
       </SidebarFooter>
     </Sidebar>
-  )
+  );
 }
